51de77ca1e4fd9507f8a5076c0c23ed339491b12
Enid Medina <medina.enid@gmail.com>
2017-11-15 15:11:49 -0600

    frnt

---
+++ web/js/pop-up.js
@@ -0,0 +1,5 @@
+function Abrir_ventana (pagina) {
+    var opciones=”toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no,          resizable=yes, width=640, height=480, top=100, left=100″;
+    
+    window.open(pagina,””,opciones);
+    }
