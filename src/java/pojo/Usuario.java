316ff9a8cde427f20027c6934d96195c61164e05
Yhair PD <bryamyhairperezdiaz@gmail.com>
2017-11-15 15:44:12 -0600

    pojo u

--- src/java/pojo/Usuario.java
+++ src/java/pojo/Usuario.java
@@ -5,10 +5,143 @@
  */
 package pojo;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  *
  * @author Yhair
  */
+@Entity
+@Table(name="usuario",catalog="whatsApp")
 public class Usuario {
+    
+    @Id @GeneratedValue
+    @Column(name="idUsuario")
+    private int idUsuario;
+    
+    @Column(name="nombre",nullable=false)
+    private String nombre;
+    
+    @Column(name="telefono",nullable=false)
+    private String telefono;
+    
+    @Column(name="conectado",nullable=false)
+    private boolean conectado;
+    
+    @Column(name="fotografia",nullable=false)
+    private String fotografia;
+    
+    @Column(name="contrasena",nullable=false)
+    private String contrasena;
+    
+    @Column(name="eliminado",nullable=false)
+    private boolean eliminado;
+    
+    public Usuario(){
+        
+    }
+
+    /**
+     * @return the idUsuaio
+     */
+    public int getIdUsuaio() {
+        return idUsuario;
+    }
+
+    /**
+     * @param idUsuaio the idUsuaio to set
+     */
+    public void setIdUsuaio(int idUsuaio) {
+        this.idUsuario = idUsuaio;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the telefono
+     */
+    public String getTelefono() {
+        return telefono;
+    }
+
+    /**
+     * @param telefono the telefono to set
+     */
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    /**
+     * @return the conectado
+     */
+    public boolean isConectado() {
+        return conectado;
+    }
+
+    /**
+     * @param conectado the conectado to set
+     */
+    public void setConectado(boolean conectado) {
+        this.conectado = conectado;
+    }
+
+    /**
+     * @return the fotografia
+     */
+    public String getFotografia() {
+        return fotografia;
+    }
+
+    /**
+     * @param fotografia the fotografia to set
+     */
+    public void setFotografia(String fotografia) {
+        this.fotografia = fotografia;
+    }
+
+    /**
+     * @return the contrasena
+     */
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    /**
+     * @param contrasena the contrasena to set
+     */
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    /**
+     * @return the eliminado
+     */
+    public boolean isEliminado() {
+        return eliminado;
+    }
+
+    /**
+     * @param eliminado the eliminado to set
+     */
+    public void setEliminado(boolean eliminado) {
+        this.eliminado = eliminado;
+    }
+    
     
 }
