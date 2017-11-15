245d25aa45e247782632a850dd7286d95d40ae72
Yhair PD <bryamyhairperezdiaz@gmail.com>
2017-11-15 15:50:30 -0600

    conflictos solve1

--- src/java/pojo/Usuario.java
+++ src/java/pojo/Usuario.java
@@ -5,32 +5,20 @@
  */
 package pojo;
 
-<<<<<<< HEAD
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
-=======
-import javax.persistence.Entity;
-import javax.persistence.Table;
+
 import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Column;
->>>>>>> ff47774a96475a6f9b5d7a8be6f019e3c20e9a55
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
 /**
  *
  * @author Francisco
  */
-<<<<<<< HEAD
-@Entity
-@Table(name="usuario",catalog="whatsApp")
-=======
 
 @Entity
 @Table(name = "usuario", catalog = "whatsApp")
->>>>>>> ff47774a96475a6f9b5d7a8be6f019e3c20e9a55
 public class Usuario {
     @Id @GeneratedValue
     @Column(name = "idUsuario", nullable = false)
@@ -157,31 +145,7 @@ public class Usuario {
     }
     
     
-    @Id @GeneratedValue
-    @Column(name="idUsuario")
-    private int idUsuario;
     
-    @Column(name="nombre",nullable=false)
-    private String nombre;
-    
-    @Column(name="telefono",nullable=false)
-    private String telefono;
-    
-    @Column(name="conectado",nullable=false)
-    private boolean conectado;
-    
-    @Column(name="fotografia",nullable=false)
-    private String fotografia;
-    
-    @Column(name="contrasena",nullable=false)
-    private String contrasena;
-    
-    @Column(name="eliminado",nullable=false)
-    private boolean eliminado;
-    
-    public Usuario(){
-        
-    }
 
     /**
      * @return the idUsuaio
@@ -197,89 +161,5 @@ public class Usuario {
         this.idUsuario = idUsuaio;
     }
 
-    /**
-     * @return the nombre
-     */
-    public String getNombre() {
-        return nombre;
-    }
 
-    /**
-     * @param nombre the nombre to set
-     */
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    /**
-     * @return the telefono
-     */
-    public String getTelefono() {
-        return telefono;
-    }
-
-    /**
-     * @param telefono the telefono to set
-     */
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-
-    /**
-     * @return the conectado
-     */
-    public boolean isConectado() {
-        return conectado;
-    }
-
-    /**
-     * @param conectado the conectado to set
-     */
-    public void setConectado(boolean conectado) {
-        this.conectado = conectado;
-    }
-
-    /**
-     * @return the fotografia
-     */
-    public String getFotografia() {
-        return fotografia;
-    }
-
-    /**
-     * @param fotografia the fotografia to set
-     */
-    public void setFotografia(String fotografia) {
-        this.fotografia = fotografia;
-    }
-
-    /**
-     * @return the contrasena
-     */
-    public String getContrasena() {
-        return contrasena;
-    }
-
-    /**
-     * @param contrasena the contrasena to set
-     */
-    public void setContrasena(String contrasena) {
-        this.contrasena = contrasena;
-    }
-
-    /**
-     * @return the eliminado
-     */
-    public boolean isEliminado() {
-        return eliminado;
-    }
-
-    /**
-     * @param eliminado the eliminado to set
-     */
-    public void setEliminado(boolean eliminado) {
-        this.eliminado = eliminado;
-    }
-    
-    
 }
