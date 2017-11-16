9c1500f7fc75a24019ecae2aaef98d2133ca2343
enriquep95 <0169321@up.edu.mx>
2017-11-16 08:54:39 -0600

    Mensaje DAO

--- src/java/dao/MensajeDAO.java
+++ src/java/dao/MensajeDAO.java
@@ -5,10 +5,49 @@
  */
 package dao;
 
+import hbn.HibernateUtil;
+import java.util.Date;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
+import pojo.Contenido;
+import pojo.Mensaje;
+import pojo.Usuario;
+
+
+
 /**
  *
- * @author Yhair
+ * @author RigoBono
  */
 public class MensajeDAO {
+    Session session;
     
+    public MensajeDAO(){
+        session=HibernateUtil.getLocalSession();
+    }
+    
+    public  Mensaje getMensajeById(int id){
+        return (Mensaje)session.load(Mensaje.class, id);
+    }
+    
+  public boolean saveMensaje(Contenido contenido,Usuario usuario){
+        Mensaje mensajeDeTanque=new Mensaje();
+        mensajeDeTanque.setDt_enviado(new Date());
+        mensajeDeTanque.setEnviado(true);
+        mensajeDeTanque.setIdContenido(contenido);
+        mensajeDeTanque.setIdUsuario(usuario);
+        try{
+            Transaction transaccion=session.beginTransaction();
+            session.save(mensajeDeTanque);
+            transaccion.commit();
+            return true;
+        }catch(Exception e){
+            
+        }finally{
+            
+        }
+        return true;
+    }
 }
